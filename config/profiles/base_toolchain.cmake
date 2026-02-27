33004b2933d3d0d5982c11e70f80896a606fde6b
Max <aaalllooollk@gmail.com>
2026-02-27 23:57:31 +0300

    Dont reinventing the wheel

--- config/profiles/base_toolchain.cmake
+++
@@ -1,4 +0,0 @@
-# Enable all configuration types on Ninja Multi-Config
-if(CMAKE_GENERATOR STREQUAL "Ninja Multi-Config")
-    set(CMAKE_CONFIGURATION_TYPES "Debug;Release;RelWithDebInfo;MinSizeRel")
-endif()
